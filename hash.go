1663e82c688fb312094a9ff5e8eddd6ed811b297
FenwickElliott <charles@fenwickelliott.io>
2018-09-25 11:38:20 -0400

    init

---
+++ hash.go
@@ -0,0 +1,23 @@
+package hash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+)
+
+// Md5 returns an md5 has of the string passed in
+func Md5(in string) string {
+	h := md5.New()
+	h.Write([]byte(in))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Md5r returns an md5 has of the interfaces passed in
+func Md5r(ins ...interface{}) string {
+	h := md5.New()
+	for _, v := range ins {
+		json.NewEncoder(h).Encode(v)
+	}
+	return hex.EncodeToString(h.Sum(nil))
+}
